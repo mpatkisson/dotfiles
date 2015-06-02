a9ff7fc3428b7a42e7f480b7857ee33db680a981
Mike Atkisson <mpatkisson@gmail.com>
2015-06-02 09:43:50 -0400

    Removed existing symlinks in setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
+if [ -L $HOME/.bash_profile ]; then 
+    rm ~/.bash_profile
+fi
 ln -s ${PWD}/bash_profile ~/.bash_profile
+
+if [ -L $HOME/.bashrc ]; then 
+    rm ~/.bashrc
+fi
 ln -s ${PWD}/bashrc ~/.bashrc
+
+if [ -L $HOME/.tmux.conf ]; then 
+    rm ~/.tmux.conf
+fi
 ln -s ${PWD}/tmux.conf ~/.tmux.conf
+
+if [ -L $HOME/.vimrc ]; then 
+    rm ~/.vimrc
+fi
 ln -s ${PWD}/vimrc ~/.vimrc
