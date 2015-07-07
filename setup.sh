f0cd0fcf46333695ab118daf38abf1a3d2fbc990
Mike Atkisson <mpatkisson@gmail.com>
2015-07-07 15:37:49 -0400

    Checked for file existence.

--- setup.sh
+++ setup.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-if [ -L $HOME/.bash_profile ]; then 
+if [ -e $HOME/.bash_profile ]; then 
     rm ~/.bash_profile
 fi
 ln -s ${PWD}/bash_profile ~/.bash_profile
 
-if [ -L $HOME/.bashrc ]; then 
+if [ -e $HOME/.bashrc ]; then 
     rm ~/.bashrc
 fi
 ln -s ${PWD}/bashrc ~/.bashrc
 
-if [ -L $HOME/.tmux.conf ]; then 
+if [ -e $HOME/.tmux.conf ]; then 
     rm ~/.tmux.conf
 fi
 ln -s ${PWD}/tmux.conf ~/.tmux.conf
 
-if [ -L $HOME/.vimrc ]; then 
+if [ -e $HOME/.vimrc ]; then 
     rm ~/.vimrc
 fi
 ln -s ${PWD}/vimrc ~/.vimrc
 
-if [ -L $HOME/.git-prompt.sh ]; then 
+if [ -e $HOME/.git-prompt.sh ]; then 
     rm ~/.git-prompt.sh
 fi
 ln -s ${PWD}/git-prompt.sh ~/.git-prompt.sh
